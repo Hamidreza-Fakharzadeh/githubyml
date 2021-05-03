699dd3ca2bf6e815ea3ee4ae6bf773386454f3a6
hamidreza <hrfarzadeh@gmail.com>
2021-05-03 17:18:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>HELLO WORLD!</p>
+      <h1>HELLO WORLD!</h1>
+      <p>This is a react app</p>
+      <p>Built up by CICD using GitHub Actions</p>
     </div>
   );
 }
